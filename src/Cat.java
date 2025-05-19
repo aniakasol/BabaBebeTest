6a988c38bf0f6c48c0d72c70c6dc5730c3894e3d
Anna Krasno <annakrasn2212@gmail.com>
2025-05-19 20:41:35 +0300

    initial

---
+++ src/Cat.java
@@ -0,0 +1,12 @@
+class CatCat {
+    public void meow(){
+        System.out.println("meow");
+    }
+}
+public class Cat {
+    public static void main(String[] args) {
+        CatCat cat = new CatCat();
+        cat.meow();
+    }
+}
+
